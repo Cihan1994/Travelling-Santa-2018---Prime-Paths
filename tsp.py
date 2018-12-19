2a8365448b10e1a31a6315e915465f2a136160ef
Cihan1994 <CDogan1994@hotmail.co.uk>
2018-12-19 16:27:52 +0000

    Add files via upload

--- tsp.py
+++ tsp.py
@@ -10,17 +10,17 @@ def calculate_fitness(dataset_tuple, population, pop_size, generation):
     # create the fitness array
     fitness_array = np.zeros(shape=pop_size)
     # loop each candidate
-    fitness = 0
     for i in range(0, population.shape[0]): # loop each candidate
+        fitness = 0
         print('calculating at generation...' + str(generation) + " for candidate..." + str(i))
         # calculate the rolling difference
         for j in range(0, population.shape[1] - 1):
             # get the distances of city one to city 2 from the population matrix
-            distance_a = dataset_tuple[population[i][j]][3]
-            distance_b = dataset_tuple[population[i][j+1]][3]
+            distance_a = dataset_tuple[int(population[i][j])][3]
+            distance_b = dataset_tuple[int(population[i][j+1])][3]
             distance = abs(distance_a - distance_b)
             fitness = fitness + distance
-        fitness_array[i] = fitness
+        fitness_array[i-1] = fitness
     return fitness_array
 
 
@@ -63,9 +63,19 @@ def mutation(selected_candidate):
 
 # a method for crossing over the candidates
 def crossover(selected_candidate_a, selected_candidate_b):
-    point_a = np.random.randint(1, selected_candidate.max())
-    point_b = np.random.randint(1, selected_candidate.max())
-    point_a_candidate = selected_candidate[point_a]
+
+    point_a = np.random.randint(1, selected_candidate_a.max())
+    point_b = np.random.randint(1, selected_candidate_b.max())
+
+    # get the chromos from a and b
+    candidate_a_a = selected_candidate_a[point_a]
+    candidate_a_b = selected_candidate_a[point_b]
+
+    # repeat for b
+    candidate_b_a = selected_candidate_b[point_a]
+    candidate_b_b = selected_candidate_b[point_b]
+
+    return candidate_a, candidate_b
 
 
 # select the candidates
@@ -101,40 +111,49 @@ if __name__ == '__main__':
     pop_size = 100
     generations = 10
     mutation_prob = 0.99
-    crossover_prob = 0.01
+    crossover_prob = 1.01
 
     # create a random population of size pop size and random cities
     population = create_population(pop_size, bits)
 
+    # fitness_record
+    best_candidates_at_gen_x = []
     # perform genetic search
     for i in range(0, generations):
-        # pass the old population for evaluation and return fitness function
-        fitness_array = calculate_fitness(dataset_tuple, population, pop_size, generation=i)
-
-        # for each candidate
+        # used to store our temporary population
+        temp_population = np.zeros(shape=(100, bits))
         for j in range(0, pop_size):
             # generate a probability between 0 - 1
             p = random.uniform(0, 1)
             if p <= mutation_prob:
-                # get the fittest candidate and its index using tournament selection
-                fittest_candidate, candidate_index = select_candidate(fitness_array, pop_size, False)
-                # return the candidate and paths
-                selected_candidate = population[candidate_index]
-                # evolvate the candidate
+                # select a random candidate
+                random_selection = random.randint(0,99)
+                selected_candidate = population[random_selection]
+                # evolve the candidate
                 evolved_candidate = mutation(selected_candidate)
-                # asign to the population array
-                population[candidate_index] = evolved_candidate
+                # assign the new candidate to our temporary population
+                temp_population[random_selection] = evolved_candidate
 
-            if p >= mutation_prob:
-                print('crossover')
-                #fittest_candidate, candidate_index = select_candidate(fitness_array, pop_size)
-                #selected_candidate = random_population[candidate_index]
+            # if p >= mutation_prob:
+            #     # select two random candidates
+            #     random_candidates = random.sample(range(100), 2)
+            #     selected_candidate_a = population[random_candidates[0]]
+            #     selected_candidate_b = population[random_candidates[1]]
+            #     # crossover the candidates
+            #     candidate_a, candidate_b = crossover(selected_candidate_a, selected_candidate_b)
+
+        # evaluate the candidates, and choose the best
+        temp_array = np.array(temp_population)
+        fitness_array = calculate_fitness(dataset_tuple, temp_array, pop_size, i)
+        fittest_candidate, candidate_index = select_candidate(fitness_array, pop_size, False)
+        population[candidate_index] = fittest_candidate
+        best_candidates_at_gen_x.append([i, fittest_candidate])
 
     # split results out to a csv # get the fittest canddiate
-        fittest_candidate, candidate_index = select_candidate(fitness_array, pop_size, True)
-        candidate = population[candidate_index]
-        candidate_df = pd.DataFrame(data=candidate, columns=['Path'])
-        candidate_df.to_csv(r'S:\CarbonTrading\IT\git\cd_scripts\Cities\cities_submission.csv', index=False)
+    fittest_candidate, candidate_index = select_candidate(fitness_array, pop_size, True)
+    candidate = population[candidate_index]
+    candidate_df = pd.DataFrame(data=candidate, columns=['Path'])
+    candidate_df.to_csv(r'S:\CarbonTrading\IT\git\cd_scripts\Cities\cities_submission.csv', index=False)
 
 
 
